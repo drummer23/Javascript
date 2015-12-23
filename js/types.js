ce4a325cb45b4f3fed000249c48845e2a0d4a8fa
Roger Spielmann <rspielmann@snowflake.ch>
2015-12-23 10:04:45 +0100

    types added

---
+++ js/types.js
@@ -0,0 +1,22 @@
+var undVar;
+console.log(typeof undVar); //-> undefined
+
+var bolVar = true;
+console.log(typeof bolVar); // -> boolean
+
+var numVar = 7;
+console.log(typeof numVar); // -> number
+
+var strVar = 'hello';
+console.log(typeof strVar); // -> string
+
+var arrVar = ['a','b','c'];
+console.log(typeof arrVar); // -> object
+
+var objVar = {a : 'a', b: 'b'};
+console.log(typeof objVar); // -> object
+
+
+
+
+
